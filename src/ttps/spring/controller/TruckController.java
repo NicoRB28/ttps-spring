8320bf65a2700ceecf4526d8d88a8d21c7b40d0f
nico <nicolasrodriguezboschi@gmail.com>
2021-02-23 00:40:34 -0300

    elimino import

--- src/ttps/spring/controller/TruckController.java
+++ src/ttps/spring/controller/TruckController.java
@@ -2,7 +2,7 @@ package ttps.spring.controller;
 
 import java.util.List;
 
-import org.hibernate.service.spi.ServiceException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
